2c296d558bc36d314d0896d8a801a9f7e4fbe659
Eloy Duran <eloy.de.enige@gmail.com>
2009-10-24 00:50:56 +0200

    Add generator-san and cache the rails checkout for testing purposes. Added a Rakefile with default task which creates a test app.

--- fingertips.rb
+++ fingertips.rb
@@ -2,17 +2,27 @@
 # git project and a closed svn one. The svn version should install gems instead
 # of creating checkouts of the git libs in vendor/plugins.
 
-# On 2.3.4 the git command is broken as it only executes in_root...
-inside 'vendor' do
-  Git.run 'clone git://github.com/Fingertips/rails.git'
-  run 'cd rails && git remote add rails git://github.com/rails/rails.git'
+if ENV['TEST_TEMPLATE'] && File.exist?('../rails')
+  puts '[!] Using rails cache...'
+  run 'cp -R ../rails vendor/'
+else
+  # On 2.3.4 the git command is broken as it only executes in_root...
+  inside 'vendor' do
+    Git.run 'clone git://github.com/Fingertips/rails.git'
+    run 'cd rails && git remote add rails git://github.com/rails/rails.git'
+  end
+  if ENV['TEST_TEMPLATE']
+    puts '[!] Creating rails cache...'
+    run 'cp -R vendor/rails ../'
+  end
 end
 
-plugin 'authorization-san',         :git => 'git://github.com/Fingertips/authorization-san.git'
 plugin 'authentication-needed-san', :git => 'git://github.com/Fingertips/authentication-needed-san.git'
+plugin 'authorization-san',         :git => 'git://github.com/Fingertips/authorization-san.git'
+plugin 'generator-san',             :git => 'git://github.com/Fingertips/generator-san.git'
 plugin 'peiji-san',                 :git => 'git://github.com/Fingertips/peiji-san.git'
 plugin 'on-test-spec',              :git => 'git://github.com/Fingertips/on-test-spec.git'
 
-gem 'test-spec', :version => '~> 0.10.0'
+gem 'test-spec', :version => '>= 0.10.0'
 
 rake("gems:install", :sudo => true)
